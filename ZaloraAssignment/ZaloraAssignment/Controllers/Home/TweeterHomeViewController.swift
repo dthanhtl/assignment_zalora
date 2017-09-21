ee2cf95cac0f12963a41fd4d5a3fd87c0777f6e2
Tran Le Duy Thanh <your_email_address@example.com>
2017-09-21 17:39:14 +0700

    validate textfield

--- ZaloraAssignment/ZaloraAssignment/Controllers/Home/TweeterHomeViewController.swift
+++ ZaloraAssignment/ZaloraAssignment/Controllers/Home/TweeterHomeViewController.swift
@@ -29,6 +29,8 @@ class TweeterHomeViewController: UIViewController {
   var ref : DatabaseReference!
   var userID = ""
   var count = 0
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -48,7 +50,7 @@ class TweeterHomeViewController: UIViewController {
           self.count += 1
         }
         self.posts = newPosts
-        self.posts.reverse()
+        self.posts.reverse() // reverse to get the latest post to top
         self.tableView.isHidden = false
         
         self.tableView.reloadData()
@@ -78,6 +80,26 @@ class TweeterHomeViewController: UIViewController {
   
   @IBAction func sendTapped(_ sender: Any) {
     
+    if (self.tfInputPost.text?.isEmpty ?? true) {
+      let alert = UIAlertController(title: "Tweeter",
+                                    message: "Please write something :)",
+                                    preferredStyle: .alert)
+      
+      
+      let cancelAction = UIAlertAction(title: "Cancel",
+                                     style: .default) { _ in
+                                      self.tfInputPost.becomeFirstResponder()
+                                      
+      }
+      
+
+      alert.addAction(cancelAction)
+      
+      present(alert, animated: true, completion: nil)
+      
+      return
+    }
+    
     let addedTime = Date().timeIntervalSince1970
     
     let dateFormatter = DateFormatter()
@@ -94,11 +116,19 @@ class TweeterHomeViewController: UIViewController {
     
     // 4
     postItemRef.setValue(postItem.toAnyObject())
+    
+    self.tfInputPost.text = ""
   }
   
   
 
 }
+
+// MARK: - functions
+
+
+
+// MARK: - table
 
 extension TweeterHomeViewController: UITableViewDelegate, UITableViewDataSource {
   
